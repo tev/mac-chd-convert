01fae221210bea8b6f064cb8755b454433b1c20f
Tev <tev@tev.net>
2022-11-20 13:10:49 -0500

    Create mac-chd-convert.sh
    added script

---
+++ mac-chd-convert.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+inputPath=$1
+# note -- use an absolute path, no tilde
+tempDir="${inputPath}_tmp"
+outDir="${inputPath}_out"
+
+if [ -d $tempDir ]
+then
+    printf "[ Temp directory exists ]"
+else
+    printf "[ Creating temp directory ]"
+    mkdir $tempDir
+fi
+
+if [ -d $outDir ]
+then
+    printf "[ Output directory exists ]"
+else
+    printf "[ Creating output directory ]"
+    mkdir $outDir
+fi
+
+printf "[ Scanning $inputPath ... ]"
+for file in $1/*.7z(.)
+do
+  filename=$file:t
+  printf "\n\n\n[ Processing ${filename} ]"
+  7z x $file -o$tempDir
+  for i in $tempDir/*.cue(.); do chdman createcd -i "$i" -o "${i%.*}.chd"; done
+  mv $tempDir/*.chd(.) $outDir
+  rm -rf $tempDir/*
+done
+rm -rf $tempDir
